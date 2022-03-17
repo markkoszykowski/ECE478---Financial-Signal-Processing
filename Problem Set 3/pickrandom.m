6af1501df819546b6123970b9e3f36363948696c
markkoszykowski <markkoszykowski@gmail.com>
2022-03-16 23:52:05 -0400

    completed ps3

--- Problem Set 3/pickrandom.m
+++ Problem Set 3/pickrandom.m
@@ -1,6 +1,6 @@
 function [SN] = pickrandom(Sn, Pn, tolerance)
     assert(abs(sum(Pn) - 1) <= tolerance, "Probabilities are not valid.");
-    assert(all(size(Sn) == size(Pn)), "Values and probabilities vectors do not align");
+    assert(all(size(Sn) == size(Pn), "all"), "Values and probabilities vectors do not align");
     Cn = [0 cumsum(Pn)];
     SN = Sn(find(rand > Cn, 1, "last"));
 end
